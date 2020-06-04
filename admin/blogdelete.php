81f7039acf5daa3e37b86d07c7da289019afe568
Marsh Vishal <vishal.lobo@hashedin.com>
2020-06-04 21:47:39 +0530

    blog delete script modified

--- admin/blogdelete.php
+++ admin/blogdelete.php
@@ -1,10 +1,12 @@
 <?php
 if (isset($_POST['submit'])){
     $id = $_POST['id'];
+    $image = $_POST['imagename'];
     include_once('database/queries.php');
     $qry = new Querry();
     $result = $qry->deleteblogs($id);
-    header('Location:/');
+    unlink('img/'.$image.'');
+    header('Location:index.php');
     exit();
 
 }
