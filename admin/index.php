64c566dc8c7c25701eafc3a349bf1b613dbac3ba
Marsh Vishal <vishal.lobo@hashedin.com>
2020-06-04 14:42:12 +0530

    basic template

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Admin Panel </title>
+    <style>
+        .container{
+            display: flex;
+            justify-content:space-between;
+        }
+        .newblog{
+            width:400px;
+            margin-right: 10%;
+            box-shadow: 3px 3px 5px 6px #ccc;
+            text-align: center;
+            
+        }
+        .head{
+            margin-left: 5%;
+            
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="head" >
+            <h2  >Admin Panel</h2>
+        </div>
+        <div class="newblog">
+            <button style="margin-top: 5%;"><a href="newblog.php">POST NEW BLOG</a></button>
+        </div>
+
+    </div>
+
+    </boby>
+
+</html>
