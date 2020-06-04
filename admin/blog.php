2fa8eefbe3d5c4e5b99028c6b17205be007020bb
Marsh Vishal <vishal.lobo@hashedin.com>
2020-06-04 18:32:53 +0530

    image compressing if the size is more than 500kb saperate function added for data upload

--- admin/blog.php
+++ admin/blog.php
@@ -1,40 +1,55 @@
 <?php
-if(isset($_POST['submit'])){
-    $imagename = pathinfo(basename($_FILES["blogimage"]["name"]),PATHINFO_FILENAME);
-    $imageext = strtolower(pathinfo(basename($_FILES["blogimage"]["name"]),PATHINFO_EXTENSION));
-    if ($imageext !=='jpg' && $imageext !=='png' && $imageext !=='jpeg' && $imageext !=='gif' ){
+function dataupload($title, $image, $date, $content)
+{
+    include_once('database/queries.php');
+    $qry = new Querry();
+    $result = $qry->newblog($title, $image, $date, $time, $content);
+    if ($result) {
+        header('Location:index.php?success=New Blog is Posted');
+        exit();
+    } else {
+        unlink($path);
+        header('Location:newblog.php?error=There was an error uploading file');
+        exit();
+    }
+}
+function compressimage($source, $path, $quality)
+{
+    $info = getimagesize($source);
+    if ($info['mime'] == 'image/jpeg')
+        $image = imagecreatefromjpeg($source);
+    if ($info['mime'] == 'image/gif')
+        $image = imagecreatefromgif($source);
+    if ($info['mime'] == 'image/png')
+        $image = imagecreatefrompng($source);
+    return imagejpeg($image, $path, $quality);
+}
+
+if (isset($_POST['submit'])) {
+    $imagename = pathinfo(basename($_FILES["blogimage"]["name"]), PATHINFO_FILENAME);
+    $imageext = strtolower(pathinfo(basename($_FILES["blogimage"]["name"]), PATHINFO_EXTENSION));
+    if ($imageext !== 'jpg' && $imageext !== 'png' && $imageext !== 'jpeg' && $imageext !== 'gif') {
         header('Location:newblog.php?error=File is not supported');
         exit();
-    }
-    else{
-        $time=time();
+    } else {
+        $time = time();
         $title = $_POST['title'];
         $content  = $_POST['content'];
-        $image = $imagename.$time.".".$imageext;
-        $path = 'img/'.$image;
-        if(move_uploaded_file($_FILES["blogimage"]["tmp_name"],$path)){
-            include_once('database/queries.php');
-            $qry = new Querry();
-            $result = $qry->newblog($title,$image,date("d-M-Y",$time),$content);
-            if ($result)
-            {
-                header('Location:index.php?success=New Blog is Posted');
-                exit();  
+        // concat with date time to resolve overwrite isssues 
+        $image = $imagename . $time . "." . $imageext;
+        $path = 'img/' . $image;
+        if ($_FILES["blogimage"]["size"] > 500000) {
+            // if the size of image is more than 500kb compressing it
+            if (compressimage($_FILES["blogimage"]["tmp_name"], $path, 50)) {
+                //uploading the data to data base
+                dataupload($title, $image, date("d-M-Y", $time), $content);
             }
-            else{
-                unlink($path);
-                header('Location:newblog.php?error=There was an error uploading file');
-                exit();  
-            }
-
+        } else if (move_uploaded_file($_FILES["blogimage"]["tmp_name"], $path)) {
+            dataupload($title, $image, date("d-M-Y", $time), $content);
+        } else {
+            $error = 'There was an error uploading file' . $path . '';
+            header('Location:newblog.php?error=' . $error . '');
+            exit();
         }
-        else{
-            $error ='There was an error uploading file'.$path.'';
-            header('Location:newblog.php?error='.$error.'');
-        exit();
-        }
-        
     }
-    
 }
-?>
