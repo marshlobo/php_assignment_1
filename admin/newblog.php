773a1e3a35c85e868e5eb2196c754dd253258f1f
Marsh Vishal <vishal.lobo@hashedin.com>
2020-06-04 16:28:37 +0530

    code reafactored

--- admin/newblog.php
+++ admin/newblog.php
@@ -10,32 +10,37 @@
     });
   </script>
   <style>
-    .container{
+    .container {
       margin-left: 20%;
       margin-top: 10%;
       box-shadow: 3px 3px 5px 6px #ccc;
-      width:60%;
-      
+      width: 60%;
+
     }
   </style>
 </head>
 
 <body>
   <div class="container">
-    <h2 style="text-align: center; ">POST NEW BLOG</h2>
-    <form  style="margin-left: 1%; " action="blog.php" method="POST">
-      <div>
-        <input style="margin-top: 1%;"type="file" name="blogimage" required>
-        <br />
-        <br />
-        <input type="text" name="title" style="width: 90%;" placeholder="TITLE">
-        <br />
-        <br />
-        CONTENT:
-        <textarea name="content" style="width: 90%; height: 100px;" ></textarea>
-        <br />
-        <br />
-        <button  style="margin-bottom: 1%;" name="submit" type="submit">POST</button>
+    <h2 style="text-align: center; ">POST NEW BLOG </h2>
+    <div style="margin-left: 20%; color:red; ">
+    <?php 
+      if(isset($_GET['error']))
+      echo $_GET['error'];
+          ?>
+    </div>
+    <form style="margin-left: 1%; " action="blog.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="blogimage" id="blogimage" required>
+      <br />
+      <br />
+      <input type="text" name="title" style="width: 90%;" placeholder="TITLE">
+      <br />
+      <br />
+      CONTENT:
+      <textarea name="content" style="width: 90%; height: 100px;"></textarea>
+      <br />
+      <br />
+      <button style="margin-bottom: 1%;" name="submit" type="submit">POST</button>
     </form>
   </div>
   </boby>
